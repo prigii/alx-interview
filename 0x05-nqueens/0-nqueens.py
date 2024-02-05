5d41736fafb52fec9413590b19e6f84dee3ce267
Peter Rigii <theonlyrigii@gmail.com>
2024-02-05 20:38:13 +0300

    N queens

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+import sys
+
+"""
+Solving the N queens puzzle 
+"""
+
+
+def is_safe(board, row, col, n):
+    # Check if there is a queen in the same row
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+
+    # Check upper diagonal on left side
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    # Check lower diagonal on left side
+    for i, j in zip(range(row, n), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+def solve_nqueens_util(board, col, n):
+    if col >= n:
+        print_solution(board)
+        return True
+
+    res = False
+    for i in range(n):
+        if is_safe(board, i, col, n):
+            board[i][col] = 1
+            res = solve_nqueens_util(board, col + 1, n) or res
+            board[i][col] = 0  # backtrack
+
+    return res
+
+def solve_nqueens(n):
+    if not n.isdigit():
+        print("N must be a number")
+        sys.exit(1)
+
+    n = int(n)
+
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [[0 for _ in range(n)] for _ in range(n)]
+
+    if not solve_nqueens_util(board, 0, n):
+        print("No solution found")
+
+def print_solution(board):
+    solution = []
+    for i in range(len(board)):
+        queen_pos = [i, board[i].index(1)]
+        solution.append(queen_pos)
+    print(solution)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    solve_nqueens(sys.argv[1])
